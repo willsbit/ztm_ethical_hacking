6e8816bb026d24ee033dbf069058ea639abe8b69
Willian <willsbit@gmail.com>
2022-03-03 15:28:35 -0300

    Creating a portscanner in python3

--- portscanner/portscanner.py
+++ portscanner/portscanner.py
@@ -1,5 +1,28 @@
 import socket
 import termcolor
 
-sock = socket.socket()
-sock.connect((ipaddres, port))
+
+def scan(target, ports):
+    for port in range(1, ports):
+        scan_port(target, port)
+
+
+def scan_port(ipaddress, port):
+    try:
+        sock = socket.socket()
+        sock.connect((ipaddress, port))
+        print(f"[+] Port {str(port)} opened")
+        sock.close()
+    except:
+        print(f"[-] Port {str(port)} closed")
+
+
+targets = input("[*] Enter targets to scan (separated by ,):")
+ports = input("[*] How many ports to scan (int):")
+
+if ',' in targets:
+    print("[*] Scanning multiple targets")
+    for target in targets.split(','):
+        scan(target.strip(), ports)
+else:
+    scan(targets, ports)
